c95f381c4788e59353bf4e11fc07097bfd669649
“Brandon <BaanduRv@gmail.com>
2024-11-05 00:46:18 -0500

    web construccion page

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  base: "/xor_landing_page/"
+});
